283d759875450271b2b93bf217f67f86b8097f88
Michal <michalgrzegorczyk04@gmail.com>
2022-11-22 08:12:16 +0100

    Create own factory

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'tags' => 'api, laravel, backend',
+            'company' => $this->faker->company(),
+            'email' => $this->faker->companyEmail(),
+            'website' => $this->faker->url,
+            'location' => $this->faker->city(),
+            'email' => $this->faker->companyEmail(),
+            'description' => $this->faker->paragraph(5),
+        ];
+    }
+}
