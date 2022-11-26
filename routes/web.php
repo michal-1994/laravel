8545b39ec735a1932ebd19a311b845d92861d0f9
Michal <michalgrzegorczyk04@gmail.com>
2022-11-26 15:17:35 +0100

    Add delete function

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,9 @@ Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 // Update Listing
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
 
 
 
